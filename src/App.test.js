467566f45f599bf949f9a670fc5cfcd167824b2d
jordan <jordan.craven@quartech.com>
2021-11-12 17:52:44 -0800

    add some todos for tests...

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,25 @@ test('renders header text', () => {
   const linkElement = getByText(/toast exercise/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('content header text to be initially be loading', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/loading.../i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('header to contain the new submission button', () => {
+  const { getByText } = render(<App />);
+  const btn = getByText(/new submission/i);
+  expect(btn).toBeInTheDocument();
+});
+
+test.todo('clicking new submission shows a notification');
+
+test.todo('clicking like on a notification updates header status to saving'); 
+
+test.todo('clicking close dismisses it');
+
+test.todo('when a notification is liked, it is added to the liked list');
+
+test.todo('can load liked submissions after saving one');
